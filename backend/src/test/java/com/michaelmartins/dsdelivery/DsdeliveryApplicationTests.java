6aeba56756df455095a4d7c849af6961a1f57093
Michael <michael.dl.mrtns@gmail.com>
2021-01-04 15:52:29 -0300

    Project Created

---
+++ backend/src/test/java/com/michaelmartins/dsdelivery/DsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelmartins.dsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
